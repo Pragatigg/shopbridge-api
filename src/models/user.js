a139a7a25a1b498b5bbaa497e89ebed67a15e468
Pragati Garud <pragati.garud.vc@startv.com>
2021-07-29 20:09:05 +0530

    toJOSN method use

--- src/models/user.js
+++ src/models/user.js
@@ -33,7 +33,17 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
-//Instance method
+//Instance methods
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObj = user.toObject();
+
+    delete userObj.password;
+    delete userObj.tokens;
+
+    return userObj; 
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "secret");
